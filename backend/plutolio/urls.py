e3c88741cde0cb1c726a3927a2ca49837fee820c
barkar19 <barkar19@gmail.com>
2020-04-20 21:10:24 +0200

    Added vuetify

--- backend/plutolio/urls.py
+++ backend/plutolio/urls.py
@@ -20,6 +20,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('positions/',TemplateView.as_view(template_name='index.html'))
+    path('api/', include(router.urls))
 ]
