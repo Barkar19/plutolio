903ffb0e27cd8d82ec3abd2b6b656539f6d052d4
barkar19 <barkar19@gmail.com>
2020-04-23 20:35:57 +0200

    NavBar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,60 +1,40 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+
+    <NavBar v-bind:drawer_value="drawer"/>
+
     <v-app-bar
       app
-      color="primary"
-      dark
+      clipped-left
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Plutolio</v-toolbar-title>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import NavBar from './components/NavBar.vue'
 
-export default {
-  name: 'App',
+  export default {
+    props: {
+      source: String,
+    },
+    components: {
+      NavBar
+    },
+    data: () => ({
+      drawer: null,
+    }),
 
-  components: {
-    HelloWorld,
-  },
+    created () {
+      this.$vuetify.theme.dark = true
+    },
 
-  data: () => ({
-    //
-  }),
-};
+    
+  }
 </script>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-navigation-drawer
+      v-model="drawer_value"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link v-for="l in links" :key="l.route">
+          <v-list-item-action>
+            <v-icon>{{ l.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ l.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    props: ["drawer_value"],
+    data() {
+        return {
+            links: [
+                {icon: "account_balance_wallet", text: "Home", route:"/"},
+                {icon: "insert_chart_outlined", text: "Reports", route:"/reports"},
+                {icon: "transform", text: "Transactions", route:"/transactions"},
+                {icon: "euro", text: "Currencies", route:"/currencies"},
+                {icon: "settings", text: "Settings", route:"/settings"},
+                
+            ]
+        }
+    }
+}
+</script>
